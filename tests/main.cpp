0f46b686ed5c4f3952a416aec47552301c48e6c2
Mark Riddoch <mark@dianomic.com>
2023-07-25 07:39:40 +0000

    FOGL-7023 set PYTHONPATH
    Signed-off-by: Mark Riddoch <mark@dianomic.com>

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,6 +8,8 @@ using namespace std;
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
 
+    setenv("PYTHONPATH", ".", 1);
+
     testing::GTEST_FLAG(repeat) = 200;
     testing::GTEST_FLAG(shuffle) = true;
     testing::GTEST_FLAG(death_test_style) = "threadsafe";
